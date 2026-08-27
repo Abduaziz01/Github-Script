8cacf1f1fb08f80daccb43b2ee56d3d1b9035a68
Cosmic Fear <Abduazizbuba@gmail.com>
2026-08-27 15:00:01 +0500

    Day 164: line 164/641

--- new.py
+++ new.py
@@ -161,3 +161,4 @@ def create_repo(token: str, repo: str) -> bool:
     )
     return resp is not None and resp.status_code == 201
 
+def get_file_sha(token: str, login: str, repo: str, filename: str) -> str | None:
