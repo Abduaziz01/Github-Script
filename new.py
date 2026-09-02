fc3778314671ff9f6325ce6844ac314d345f52d7
Cosmic Fear <Abduazizbuba@gmail.com>
2026-09-02 15:00:01 +0500

    Day 170: line 170/641

--- new.py
+++ new.py
@@ -167,3 +167,4 @@ def get_file_sha(token: str, login: str, repo: str, filename: str) -> str | None
     )
     if resp and resp.status_code == 200:
         return resp.json().get("sha")
+    return None
