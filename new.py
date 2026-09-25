877629fcda044926879fce95b7f4f3dad71a5f28
Cosmic Fear <Abduazizbuba@gmail.com>
2026-09-25 15:00:01 +0500

    Day 193: line 193/641

--- new.py
+++ new.py
@@ -190,3 +190,4 @@ def push_file(token: str, login: str, repo: str, filename: str,
     if resp is None:
         return False, "Нет соединения с GitHub"
     if resp.status_code in (200, 201):
+        return True, ""
